efb7630745b341bf5942f155e574265d9923cb6c
Lisa <lisa.plaseski@gmail.com>
2026-03-17 02:41:54 -0400

    Make modal slight more usable on mobile

--- src/common/YoutubeModal.tsx
+++ src/common/YoutubeModal.tsx
@@ -20,15 +20,18 @@ const Modal = ({ videoUrl, onClose }: ModalProps) => {
     };
   }, [onClose]);
 
+  const handleClose = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+    onClose();
+  };
+
   return (
     <div className='fixed inset-0 z-50 flex items-center justify-center'>
       <div className='absolute inset-0 bg-black opacity-50' onClick={onClose} />
-      <div className='relative m-20 aspect-video h-9/10 rounded-sm bg-white p-6 shadow-lg'>
+      <div className='media relative m-20 aspect-video h-9/10 max-w-full rounded-sm bg-white p-2 shadow-lg lg:p-6'>
         <button
-          onClick={(e) => {
-            e.stopPropagation();
-            onClose();
-          }}
+          aria-label='close video'
+          onClick={handleClose}
           className='absolute -top-10 -right-10 h-10 w-10 cursor-pointer rounded-full bg-white p-1 text-gray-500 shadow-2xl hover:font-bold hover:text-indigo-800'
         >
           ✕
@@ -42,6 +45,12 @@ const Modal = ({ videoUrl, onClose }: ModalProps) => {
             allowFullScreen
           ></iframe>
         </div>
+        <button
+          className='color-black relative top-4 rounded-md bg-gray-300 p-1 text-sm font-medium hover:bg-gray-400 2xl:hidden'
+          onClick={handleClose}
+        >
+          Close Video
+        </button>
       </div>
     </div>
   );
